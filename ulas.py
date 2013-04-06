45495c8ae2d21eea15b885902280013264b261e7
İsmail Dönmez <idoenmez@suse.de>
2013-04-06 13:39:25 +0200

    Give a JSON output

--- ulas.py
+++ ulas.py
@@ -5,6 +5,7 @@
 # 2013 İsmail Dönmez <ismail@donmez.ws>
 
 from collections import OrderedDict
+import json
 from lxml import html
 import urllib2
 import re
@@ -14,12 +15,13 @@ import time
 address="http://www.burulas.com.tr/sayfa.aspx?id=393"
 base_url="http://www.burulas.com.tr"
 clock_dict = OrderedDict()
+timeTableDict = {}
 
 def compareTime(t1, t2):
-    h,m = t1.split(":")
+    h,m = t1.split(" ")[0].split(":")
     vt1 = int(h)*60 + int(m)
 
-    h,m = t2.split(":")
+    h,m = t2.split(" ")[0].split(":")
     vt2 = int(h)*60 + int(m)
 
     return (vt1 > vt2)
@@ -35,7 +37,7 @@ def splitTimeTable(hours):
         timeTable[len(timeTable)-1].append(currentHour)
         prevHour = currentHour
 
-    print timeTable
+    return timeTable
 
 def parseTable(table):
     for row in table.xpath("//tbody/tr"):
@@ -45,25 +47,19 @@ def parseTable(table):
             content = content.replace("\xc2\xa0","")
             hours = content.replace("\t","").split("\r\n")
             timeTable = [hour.strip() for hour in hours if hour != '']
-            splitTimeTable(timeTable)
+            return splitTimeTable(timeTable)
             break
 
 def parsePage(doc):
     tableList = doc.xpath("/html//table")
-    dataFound = False
 
     for table in tableList:
-        if dataFound:
-            break
-            
         try:
             for key in table.attrib['style'].split(";"):
                 if key.find("width") >= 0:
                     width = key.split(":")[1].strip().rstrip("px;")
                     if width.find("%") < 0:
-                        parseTable(table)
-                        dataFound = True
-                        break
+                        return parseTable(table)
         except KeyError:
             pass
 
@@ -80,12 +76,19 @@ def setupBus():
 def setupTimeline():
     for key in clock_dict.keys():
         url = clock_dict[key]
-        print url
         doc = html.fromstring(urllib2.urlopen(url).read())
-        parsePage(doc)
+
+        if not key in timeTableDict.keys():
+            timeTableDict[key] = {}
+            timeTableDict[key]["url"] = None
+            timeTableDict[key]["hours"] = None
+
+        timeTableDict[key]["url"] = url
+        timeTableDict[key]["hours"] = parsePage(doc)
+        print json.dumps(timeTableDict, sort_keys=True,
+                                        indent=4, separators=(',', ': '))
         time.sleep(5)
 
 if __name__ == "__main__":
     setupBus()
     setupTimeline()
-
