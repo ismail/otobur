7eec1c8b4eb3757e9d5bdb4f01859ecf3daf2f2e
İsmail Dönmez <ismail@donmez.ws>
2013-04-11 12:32:23 +0200

    Cleanup imports

--- burulas/src/ws/donmez/burulas/DetailsSlideActivity.java
+++ burulas/src/ws/donmez/burulas/DetailsSlideActivity.java
@@ -2,16 +2,14 @@ package ws.donmez.burulas;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 
 public class DetailsSlideActivity extends FragmentActivity {
-    
+
     private static final int NUM_PAGES = 3;
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
