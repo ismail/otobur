14e93c88ea9ea60239a34213f46dba628072022b
İsmail Dönmez <ismail@donmez.ws>
2013-06-06 17:27:23 +0200

    Update script

--- scripts/ulas2json.py
+++ scripts/ulas2json.py
@@ -4,29 +4,54 @@
 # Placed under public domain.
 # 2013 İsmail Dönmez <ismail@donmez.ws>
 
-from lxml import etree,html
+from collections import OrderedDict
+from lxml import etree, html
 import json
 import urllib2
 import sys
 
 root = "http://www.burulas.com.tr/hatsorgulama.aspx?hatAdi=99999"
 baseUrl = "http://www.burulas.com.tr/hareketSaatleri.aspx"
-timeTableDict = {}
+scheduleDict = {}
 
 def parseSchedule(busName, address):
     tree=html.fromstring(urllib2.urlopen(address).read())
 
+    stopName = None
     for td in tree.xpath('//table[@class="brTable"][1]//td'):
         if td.attrib.get("colspan") in ["1", "2", "3", "4"] and not td.attrib.get("cellpadding"):
             if td.attrib.get("style") == "text-align:justify;padding:5px;":
-                route = td.text_content().strip().split("-")
-                route = [x.strip() for x in route]
-                print "Route: %s" % route
+                route = td.text_content().strip().encode("utf-8")
+
+                keyword = None
+
+                if route.find("DÖNÜŞÜNDE") >= 0:
+                    pass
+                elif route.find("DÖNÜŞ:") >= 0:
+                    keyword = "DÖNÜŞ:"
+                elif route.find("DÖNÜŞ :") >= 0:
+                    keyword = "DÖNÜŞ :"
+
+                if keyword:
+                    if busName == "F/3": # XXX hackity hack
+                        (forward, foo, backward) = route.split(keyword)
+                    else:
+                        (forward, backward) = route.split(keyword)
+
+                    forward = [x.strip() for x in forward.split("-")]
+                    backward = [x.strip() for x in backward.split("-")]
+                else:
+                    forward = [x.strip() for x in route.split("-")]
+                    backward = None
+
+                scheduleDict[busName]["forward"] = forward
+                scheduleDict[busName]["backward"] = backward
+
             elif td.attrib.get("style") == None:
                 stopName = td.text_content()
                 stopName = stopName.encode("utf-8")
-                if stopName.find("TARİFE NO") < 0 and stopName.find("NOT:") < 0:
-                    print "Stop: %s" % stopName
+                if stopName.find("TARİFE NO") >= 0 or stopName.find("NOT:") >= 0:
+                    pass
         elif td.attrib.get("valign") == "top" and td.attrib.get("style") == "padding:5px;":
             day = td[0].text_content()
             hours = []
@@ -39,8 +64,8 @@ def parseSchedule(busName, address):
                     hours.append(hour)
                 el = el.getnext()
 
-            print "Day: %s" % day.encode("utf-8")
-            print "Hours: %s" % hours
+            stopDayName = "%s - [%s]" % (stopName, day.encode("utf-8"))
+            scheduleDict[busName]["hours"][stopDayName] = hours
 
             if not hours:
                 print "Error!"
@@ -53,9 +78,20 @@ def parseBusList():
         if target.find("hareketSaatleri.aspx") >= 0:
             address = "%s?%s" % (baseUrl, target.split("?")[1].encode("utf-8"))
             busName = target.split("hat=")[1].encode("utf-8")
-            print "######################## %s - %s ########################" % (busName, address)
+
+            scheduleDict[busName] = OrderedDict()
+            scheduleDict[busName]["forward"] = None
+            scheduleDict[busName]["backward"] = None
+            scheduleDict[busName]["url"] = address
+            scheduleDict[busName]["hours"] = OrderedDict()
+
+            print "%s - %s" % (busName, address)
             parseSchedule(busName, address)
 
 if __name__ == "__main__":
     parseBusList()
 
+    fp = open("hours.json","wb")
+    fp.write(json.dumps(scheduleDict, sort_keys=False,
+                        indent=4, separators=(',', ': ')))
+    fp.close()
