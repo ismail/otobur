1a0642a4d0b3048946a1f1a120383c12bf008a22
İsmail Dönmez <ismail@donmez.ws>
2013-07-06 16:17:40 +0200

    Add a json model

---
+++ scripts/json-model/hatlar.sh
@@ -0,0 +1,13 @@
+Tüm hatlar:
+
+http://www.bursa.bel.tr/mobil/json.php?islem=hatlar
+
+Hat Detayı:
+
+http://www.bursa.bel.tr/mobil/json.php?islem=hat_durak&hat=1/A
+
+Duraktan geçiş saati:
+
+http://www.bursa.bel.tr/mobil/json.php?islem=durak_saatler&durak=D0432&hat=1/A
+
+
