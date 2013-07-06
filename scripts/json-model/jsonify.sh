8a215ac970c4e39975b735e045423d603a216121
İsmail Dönmez <ismail@donmez.ws>
2013-07-06 16:28:55 +0200

    Update

---
+++ scripts/json-model/jsonify.sh
@@ -0,0 +1,13 @@
+#http://www.bursa.bel.tr/mobil/json.php?islem=hatlar
+#http://www.bursa.bel.tr/mobil/json.php?islem=hat_durak&hat=1/A
+#http://www.bursa.bel.tr/mobil/json.php?islem=durak_saatler&durak=D0432&hat=1/A
+
+linesURL="http://www.bursa.bel.tr/mobil/json.php?islem=hatlar"
+
+data=`curl -s $linesURL|iconv -f iso8859-9 -t utf-8`
+
+cat << EOF | node | json_pp
+a=$data
+process.stdout.write(JSON.stringify(a))
+EOF
+
