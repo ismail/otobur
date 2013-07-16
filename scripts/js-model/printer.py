865bdf1253b2ed74dbd34b9ba63f43c25d5eb881
İsmail Dönmez <ismail@donmez.ws>
2013-07-16 12:51:44 +0200

    Add a printer app

---
+++ scripts/js-model/printer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import otobur_pb2
+import sys
+
+if __name__ == "__main__":
+    schedule = otobur_pb2.Schedule()
+    schedule.ParseFromString(open(sys.argv[1],"rb").read())
+
+    for line in schedule.lines:
+        print("####################################")
+        for stop in line.stops:
+            print("%d - %d (%s %s)" % (stop.order, stop.direction, stop.name, line.name))
+        print("####################################")
+
