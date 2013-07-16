1254c47728a34ab4799db6483d10b16f31f77363
İsmail Dönmez <ismail@donmez.ws>
2013-07-16 15:55:25 +0200

    use xz

--- scripts/js-model/update.sh
+++ scripts/js-model/update.sh
@@ -4,6 +4,5 @@ time ./js2schedule.py &&
 ./sorter.py schedule.data &&
 ./printer.py sorted.data > schedule.txt &&
 mv sorted.data schedule.data &&
-rm schedule.data.lzma &&
-
-java -classpath .:$PWD/../../android/libs/lzma.jar: SevenZip.LzmaAlone e schedule.data schedule.data.lzma
+rm schedule.data.xz &&
+xz -6e schedule.data
