bbf3c1043d8a4005209559826846a86f1b0e7eb8
İsmail Dönmez <ismail@donmez.ws>
2013-07-17 11:21:17 +0200

    Use rm -f

--- scripts/js-model/update.sh
+++ scripts/js-model/update.sh
@@ -4,5 +4,5 @@ time ./js2schedule.py &&
 ./sorter.py schedule.data &&
 ./printer.py sorted.data > schedule.txt &&
 mv sorted.data schedule.data &&
-rm schedule.data.xz &&
+rm -f schedule.data.xz &&
 xz -6e schedule.data
